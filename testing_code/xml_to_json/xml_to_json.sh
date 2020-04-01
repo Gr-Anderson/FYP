cd0154f5395e70ddde47b8b9052ee9d02304a927
Sam Power <samp25@gmail.com>
2020-04-01 15:13:24 +0100

    starting processing json files

--- testing_code/xml_to_json/xml_to_json.sh
+++ testing_code/xml_to_json/xml_to_json.sh
@@ -1,28 +1,34 @@
 #!/bin/sh
 
-# finds the starting line for the ecg signal
-startline=`grep "path clip" svg_image_test.svg -n | awk -F":" '{print $1+1}'`
+filename="v102s.svg"
+json_filename="v102s.json"
+## finds the starting line for the ecg signal
+startline=`grep "path clip" sampledata/$filename -n | awk -F":" '{print $1+1}'`
 
 # finds where the ecg signal stops
-endline=`tail -n +$startline svg_image_test.svg | grep -n "L [0-9]" | tail -1 | awk -F":" '{print $1}'`
+endline=`tail -n +$startline sampledata/$filename | grep -n "L [0-9]" | tail -1 | awk -F":" '{print $1}'`
 
 # using startline and endline, crops the entire ecg signal out of the svg file
-tail +$startline svg_image_test.svg | head -$endline > tempfile
+tail +$startline sampledata/$filename | head -$endline > tempfile
 
 # creates the json file by starting an array
-echo "[" > ecg.json
+echo "{" > $json_filename
+echo "\t\"position\":" >> $json_filename
+echo "\t\t[" >> $json_filename
+
 
 # converts the ecg signal into json format
-sed 's/L /\{/g' tempfile | sed 's/ /, /g' | sed 's/, $/\},/g' >> ecg.json
+sed 's/L /\t\t\t\{\"x\"\:/g' tempfile | sed 's/ /, \"y\"\:/g' | sed 's/, \"y\"\:$/\},/g' >> $json_filename
 
 # ammends the last line of the file to a variable
-ammendedLastLine=`tail -1 ecg.json | sed 's/,$//'`
+ammendedLastLine=`tail -1 $json_filename | sed 's/,$//'`
 
 # removes the last line of the json file
-sed -i '$d' ecg.json
+sed -i '$d' $json_filename
 
 # adds the ammended line back to the json file
-echo $ammendedLastLine >> ecg.json
+echo "$ammendedLastLine" >> $json_filename
 
 # closes the array on the json file
-echo "]" >> ecg.json
+echo "\t\t]" >> $json_filename
+echo "}" >> $json_filename
